b08cd792f1ada308e22b470797776ec9168b3569
Amit Kumar <kumaramit_ak@hcl.com>
2018-10-23 15:23:07 +0530

    added a test comment to index file

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-app.get('/',(req, res) => res.send('Hell-0'))
+app.get('/users/:userId/books/:bookId', function (req, res) {
+    res.send(req.params)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
     console.log('another console log')
 
 })
+
+// test commentgit
